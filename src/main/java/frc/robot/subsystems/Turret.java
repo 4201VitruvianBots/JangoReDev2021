a69e3f02518fa9317bdb28b30d3b947655f88ef7
Malcolm <malcolm_vincent@student.davincischools.org>
2020-09-08 19:34:00 -0700

    Debug

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -8,16 +8,28 @@
 package frc.robot.subsystems;
 
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Turret extends SubsystemBase {
   /**
    * Creates a new Turret.
    */
-  public Turret() {
+   private CANSparkMax turretMotor =  new CANSparkMax(Constants.turretMotor, MotorType.kBrushless);
+   
+   public Turret() {
+    turretMotor.restoreFactoryDefaults();
+    turretMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    turretMotor.setInverted(false);
+   }
+public void Zetoutput(double output) {
+  turretMotor.set(output);
+}
 
-  }
+  
 
   @Override
   public void periodic() {
