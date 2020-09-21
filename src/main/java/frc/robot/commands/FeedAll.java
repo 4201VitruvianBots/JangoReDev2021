8b04da9381a915272490e0c7cb0d8082d3f2f35e
Sheraz4201 <Sheraz24132@gmail.com>
2020-09-20 21:33:02 -0700

    EjectAll/FeedAll/SetIndexer.Java

--- src/main/java/frc/robot/commands/FeedAll.java
+++ src/main/java/frc/robot/commands/FeedAll.java
@@ -1,23 +1,34 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+package frc.robot.commands.indexer;
+
 import frc.robot.subsystems.Indexer;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class SetIndexer extends CommandBase {
+/**
+ * An example command that uses an example subsystem.
+ */
+public class FeedAll extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final Indexer m_indexer;
-  private double m_output;
-
-  public SetIndexer(Indexer indexer, double output) {
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  double m_setpoint;
+  private double startTime;
+  public FeedAll(Indexer indexer) {
     m_indexer = indexer;
-    m_output = output;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(indexer);
+
   }
 
   // Called when the command is initially scheduled.
@@ -28,18 +39,27 @@ public class SetIndexer extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-  m_indexer.setIndexerOutput(m_output);
+    m_indexer.setIndexerOutput(0.6);
+    m_indexer.setKickerOutput(0.5);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-  m_indexer.setIndexerOutput(0);
+  public void end(final boolean interrupted) {  
+    m_indexer.setKickerOutput(0);
+    m_indexer.setIndexerOutput(0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    double time = Timer.getFPGATimestamp();
+    if(m_indexer.getIndexerTopSensor()){
+      time = Timer.getFPGATimestamp();
+    }
+    if(time >= 2)
+      return true;
+    else
+      return false;
   }
 }
