0fd5362f9a2d04ff4cd8a44cbc988c93d6af99ac
Jet <jet_pham@student.davincischools.rg>
2020-09-22 19:33:04 -0700

    Integrating Vision into the Turret

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -13,20 +13,54 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.limelight;
 
 public class Turret extends SubsystemBase {
   /**
    * Creates a new Turret.
    */
   private CANSparkMax turretMotor =  new CANSparkMax(Constants.turretMotor, MotorType.kBrushless);
-   
+  // create the limelight pretty please
+
+  // yes ^^ -jacob
+
+
   public Turret() {
     turretMotor.restoreFactoryDefaults();
     turretMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
     turretMotor.setInverted(false);
+
+    /*
+    set the camera light to on
+    turn on the camera
+    focus the lens
+    zoom the lens
+    adjust the sensitivity and exposure of the camera things. 
+    */
+
   }
+
+  /*
+  position of the robot on the field.
+  */
+
+  /*
+  the angle between the robot and the target
+  */
+
+  public double getAng(){
+    return lemonlight.getAngle();
+  }
+
   public void Zetoutput(double output) {
   turretMotor.set(output);
+  }
+
+  public void setAngle(double angle){
+    while(!(angle-1 < turretMotor.getAngle && turretMotor.getAngle < angle+1)){
+      Zetoutput(1);
+    }
+    Zetoutput(0);
   }
 
   @Override
